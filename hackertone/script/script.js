b983169aeccf80f3bd71ad039777c7cecec34c7b
Sm1le266 <71011217+Sm1le266@users.noreply.github.com>
2021-04-11 12:33:13 +0900

    ok
    asdf

--- hackertone/script/script.js
+++ hackertone/script/script.js
@@ -1,6 +1,18 @@
 let btn_file = document.querySelector('.btn_file');
 let btn_add = document.querySelector('.btn_add');
+let cab_st = document.querySelector('.cabinet_st');
+let pr = document.querySelector('.profil');
+let ex = document.querySelector('.exit');
 
+pr.style.display = 'none';
+
+cab_st.onclick = function() {
+	pr.style.display = 'block';
+}
+
+ex.onclick = function() {
+	pr.style.display = 'none';
+}
 
 btn_file.onclick = function() {
 	btn_add.disabled = false;
